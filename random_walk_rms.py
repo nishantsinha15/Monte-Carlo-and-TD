a6a059f7f040bf24f373b01e305e8e26e6a342ef
Nishant Sinha <nishant15066@iiitd.ac.in>
2018-10-07 12:50:54 +0530

    Comparison b/w td(0) learning and monte carlo done on a random walk

--- random_walk_rms.py
+++ random_walk_rms.py
@@ -12,6 +12,35 @@ def play(state):
     return s, reward
 
 
+def mc(alpha = 0.01):
+    v = np.zeros(7)
+    v_true = [0]
+    error = []
+    temp = 1
+    for i in range(1,6):
+        v[i] = 0.5
+        v_true.append(temp/6)
+        temp += 1
+    terminal_state = [0, 6]
+    for episode in range(100):
+        reward = 0
+        state = 3
+        state_list = [state]
+        while state not in terminal_state:
+            state_new, reward = play(state)
+            state_list.append(state_new)
+            state = state_new
+        g = reward
+        e = 0
+        for state in reversed(state_list):
+            if state not in terminal_state:
+                v[state] = v[state] + alpha*(g - v[state])
+                e += (v_true[state] - v[state]) ** 2
+        error.append(np.math.sqrt(e / 5))
+    # for e in error:
+    #     print(e)
+    return error
+
 def td(alpha = 0.1):
     v = np.zeros(7)
     v_true = [0]
@@ -32,19 +61,31 @@ def td(alpha = 0.1):
         for i in range(1, 6):
             e += (v_true[i] - v[i])**2
         error.append(np.math.sqrt(e / 5))
-    for e in error:
-        print(e)
+    # for e in error:
+    #     print(e)
     return error
 
 
 def main():
-    error = []
+    error1 = []
     for i in [0.15, 0.1, 0.05]:
-        e = td(i)
-        error.append(e)
-    plt.plot(np.asarray(error).T)
-    plt.show()
+        e = np.zeros(100)
+        for run in range(100):
+            e += td(i)
+        e /= 100
+        error1.append(e)
+    error2 = []
+    for i in [0.01, 0.02, 0.03, 0.04]:
+        e = np.zeros(100)
+        for run in range(100):
+            e += mc(i)
+        e /= 100
+        error2.append(e)
+    plt.plot(np.asarray(error1).T, 'b', np.asarray(error2).T, 'r')
 
 
+    plt.savefig('randomwalk2.png')
 
+
+# mc()
 main()
